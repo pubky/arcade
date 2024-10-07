5b85c28329d5fe5367446cfb812010f076040b05
amir <amir.r.amirfatahi@gmail.com>
2024-10-06 22:18:52 -0400

    Connect arcade to staging

--- src/client/setup.ts
+++ src/client/setup.ts
@@ -2,6 +2,6 @@
 
 import { PubkyClient, PublicKey } from '@synonymdev/pubky';
 
-export const client = PubkyClient.testnet();
+export const client = new PubkyClient()
 
-export const Homeserver = PublicKey.from('8pinxxgqs41n4aididenw5apqp1urfmzdztr8jt4abrkdn435ewo')
+export const Homeserver = PublicKey.from('5jsjx1o6fzu6aeeo697r3i5rx15zq41kikcye8wtwdqm4nb4tryo')
