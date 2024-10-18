25842f8a6450eb77fa00af336417806b463fba9b
amir <amir.r.amirfatahi@gmail.com>
2024-10-18 09:01:02 -0400

    Add host and port explicitly

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import commonjs from 'vite-plugin-commonjs'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), commonjs()],
+  server: {
+    host: "0.0.0.0",
+    port: 4173
+  }
 })
